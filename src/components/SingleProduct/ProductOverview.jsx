6835de4af6d8daffb40237b71a3c7a82a6e49302
Cascade-coding <fahim.real.x@gmail.com>
2026-09-19 20:28:07 +0600

    chore: adding product info

--- src/components/SingleProduct/ProductOverview.jsx
+++ src/components/SingleProduct/ProductOverview.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import ProductImageGallery from "./ProductImageGallery";
 import { useConstants } from "../../store/useConstants";
 import Container from "../shared/Container";
+import Tag from "../icons/Tag";
 
 const ProductOverview = () => {
   const product = useConstants((state) => state.DEMO_SINGLE_PRODUCT);
@@ -20,9 +20,46 @@ const ProductOverview = () => {
             <p className="font-semibold text-sm tracking-[1.44px] uppercase text-gold">
               {product.brand}
             </p>
+
             <p className="font-brand font-bold text-lg md:text-[28px] uppercase leading-snug text-brand pt-1.5">
               {product.info.title}
             </p>
+
+            <div className="select-none flex gap-x-3 gap-y-1.5 flex-wrap max-w-50 pt-3">
+              {product.info.in_stock ? (
+                <div className="px-1.5 py-1 bg-[#C9F8D7] text-success font-bold text-[11px] tracking-[0.8px] uppercase rounded-sm">
+                  IN STOCK
+                </div>
+              ) : (
+                <div className="px-1.5 py-1 bg-[#FBD5D5] text-danger font-bold text-[11px] tracking-[0.8px] uppercase rounded-sm">
+                  OUT OF STOCK
+                </div>
+              )}
+
+              {product.info.off && (
+                <div className="px-1.5 py-1 bg-[#F5EEDF] text-[#8A6A32] font-bold text-[11px] tracking-[0.8px] uppercase rounded-sm">
+                  {product.info.off} OFF
+                </div>
+              )}
+
+              {product.info.brand_new && (
+                <div className="flex gap-1 items-center px-1.5 py-0">
+                  <Tag />
+                  <span className="px-1.5 py-1 inline-block text-[#327B8A] text-sm tracking-[0.8px] uppercase">
+                    Brand New
+                  </span>
+                </div>
+              )}
+            </div>
+
+            <div className="pt-1.5">
+              {product.code && (
+                <p className="font-medium text-[13px] text-muted/70 ">
+                  Code: {product.code}
+                </p>
+              )}
+            </div>
+
             <p>{product.info.retail}</p>
             <p>{product.info.price}</p>
           </div>
